ba9b5c6c2a365beb1fd6f68d8218d42beaef55ea
Vasiliy Gorulev <89159793029@ya.ru>
2024-11-01 18:59:40 +0300

    сделал дз семинара 1

--- 01_seminar/task_2/code/index.php
+++ 01_seminar/task_2/code/index.php
@@ -1,11 +1,34 @@
 <?php
 
 $name = "Админ";
-
 var_dump($name);
-
 $name = 1;
 var_dump($name);
-
-
 var_dump([1, 2, 3]);
+
+echo "далее код к ДЗ";
+
+$a = 5;
+$b = '05';
+
+# результат в php 8.2
+## результат в php 7.4
+
+# true
+## true
+var_dump($a == $b);
+
+
+# 12345
+## 12345
+var_dump((int)'012345');
+
+
+# false
+## false
+var_dump((float)123.0 === (int)123.0);
+
+# false
+## true
+var_dump(0 == 'hello, world');
+
