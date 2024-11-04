06ce2656ac7924c4f4cdd5605c7caefa0c0f9bf0
Vasiliy Gorulev <89159793029@ya.ru>
2024-11-04 22:21:57 +0300

    добавил код с семинара 2

--- 02_seminar/task_3.php
+++ 02_seminar/task_3.php
@@ -8,5 +8,25 @@
 // "())("
 
 $str = "()()(())";
+$str2 = "())(";
 
-echo strlen($str);
+// echo strlen($str);
+
+echo validate_string($str) ? "Строка валидна" : "Ошибка в строке";
+echo validate_string($str2) ? "Строка валидна" : "Ошибка в строке";
+
+function validate_string(string $str):bool {
+    $count = 0;
+    for ($i=0; $i < strlen($str); $i++) {
+        if($str[$i] == "(") {
+            $count++;
+        } else if ($str[$i] === ")"){
+            $count--;
+        }
+        if($count < 0) {
+            return false;
+        }
+    }
+    return $count === 0;
+}
+
